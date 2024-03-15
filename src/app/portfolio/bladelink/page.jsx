6353d4190e2d601a1ba6af0a6184e5aabbde45ee
sraghuram01 <118397507+sraghuram01@users.noreply.github.com>
2024-03-14 19:25:38 -0500

    case study image gif optimization

--- src/app/portfolio/bladelink/page.jsx
+++ src/app/portfolio/bladelink/page.jsx
@@ -99,7 +99,7 @@ const BladelinkPage = () => {
       <br />
       <p>{bodyText.finalDesign}</p>
       <div className="flex justify-center">
-        <Image src={bladelinkGif} alt="Bladelink Gif" width={180} height={120} optimized/>
+        <Image src={bladelinkGif} alt="Bladelink Gif" width={180} height={120} unoptimizedoptimized/>
       </div>
       <br />
       <h2 className="font-bold text-lg">{headings.reflection}</h2>
