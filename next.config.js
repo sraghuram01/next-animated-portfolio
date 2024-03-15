c381513e39256b5b095adf2eb57a4f10c35fa550
sraghuram01 <118397507+sraghuram01@users.noreply.github.com>
2024-03-15 16:31:42 -0500

    next.config.js re configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    loader: 'default',
-    domains: ['your-domain.com'], // Replace 'your-domain.com' with your actual domain
-  },
+  // No custom image loader specified, using the default loader
 };
 
 module.exports = nextConfig;
