5b2bdf6faacebe123ac6f97d2e13bfd8ef4bf69c
sraghuram01 <118397507+sraghuram01@users.noreply.github.com>
2024-03-14 21:13:17 -0500

    absolute filepath and next config changes

--- next.config.js
+++ next.config.js
@@ -1,24 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
-// Define aliases for better import paths
-const aliases = {
-  '@public': path.resolve(__dirname, 'public'),
-};
-
-// Export the Next.js configuration
-module.exports = {
-  webpack: (config) => {
-    // Add aliases to the webpack configuration
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      ...aliases,
-    };
-    return config;
-  },
-  // Specify the image loader configuration
+const nextConfig = {
   images: {
     // Use the default loader to handle images from the /public directory
     loader: 'default',
   },
 };
+
+module.exports = nextConfig;
+
