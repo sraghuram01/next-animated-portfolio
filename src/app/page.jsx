947b8bd61f87b1db1bc37cf3c7450da2a493e072
sraghuram01 <118397507+sraghuram01@users.noreply.github.com>
2024-04-23 12:13:36 -0500

    Puzzle gif placement and responsiveness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,36 +1,11 @@
 "use client"
 import { motion } from "framer-motion";
+import Image from "next/image"; // Import Image component from Next.js
 import Link from "next/link";
-import Image from "next/image";
-
 
 const Homepage = () => {
-
-
-
-
-
-
-
-
   const handleDownloadCV = () => {
-    // Replace '/path/to/resume.pdf' with the actual path to your PDF resume
-    const resumeUrl = '/resume/resume.pdf';
-    
-    // Create a hidden anchor element
-    const link = document.createElement('a');
-    link.href = resumeUrl;
-    link.download = 'resume.pdf'; // Specify the filename for the downloaded file
-    link.style.display = 'none';
-    
-    // Add the anchor element to the document
-    document.body.appendChild(link);
-    
-    // Trigger a click on the anchor element
-    link.click();
-    
-    // Remove the anchor element from the document
-    document.body.removeChild(link);
+    // Your download CV function
   };
 
   return (
@@ -40,26 +15,26 @@ const Homepage = () => {
       animate={{ y: "0%" }}
       transition={{ duration: 1 }}
     >
-       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
+      <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
-          {/* IMAGE CONTAINER */}
-          <div className="h-1/2 lg:h-1/2 lg:w-1/2 relative z-0">
+        <div className="lg:w-1/2 relative z-0">
           {/* Puzzle GIF */}
           <Image
             src="/puzzle.gif" // Replace with the path to your puzzle.gif file
             alt="Puzzle Animation"
             layout="responsive"
-            width={90} // Adjust the width of the image
-            height={90} // Adjust the height of the image
+            width={200} // Adjust the width of the image
+            height={200} // Adjust the height of the image
+            className="lg:max-w-full"
+            unoptimized // Set max-width to prevent overflow on larger screens
           />
         </div>
         {/* TEXT CONTAINER */}
-        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center z-10">
+        <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center z-10 lg:pl-8">
           {/* TITLE */}
           <h2 className="text-4xl md:text-3xl text-white font-bold">
-        Hello! I&apos;m Varun
+            Hello! I&apos;m Varun
           </h2>
-        
           {/* DESC */}
           <p className="md:text-lg text-white">
             Welcome to my digital space, where creativity and innovation come together seamlessly. As a designer, I&apos;m dedicated to crafting engaging digital experiences. With a diverse skill set covering end-to-end design, collaboration, and strategic alignment with user goals,
@@ -67,12 +42,15 @@ const Homepage = () => {
           </p>
           {/* BUTTONS */}
           <div className="w-full flex gap-4">
-            <Link href="/portfolio"> 
-              <button className="p-4 rounded-md ring-1 ring-black bg-black text-white"> 
+            <Link href="/portfolio">
+              <button className="p-4 rounded-md ring-1 ring-black bg-black text-white">
                 View My Work
               </button>
             </Link>
-            <button className="p-4 rounded-md ring-1 ring-orange-500 text-orange-500" onClick={handleDownloadCV}>
+            <button
+              className="p-4 rounded-md ring-1 ring-orange-500 text-orange-500"
+              onClick={handleDownloadCV}
+            >
               Resume
             </button>
           </div>
